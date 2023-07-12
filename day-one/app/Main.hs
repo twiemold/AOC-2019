01f6bb44453e6c9d5cc8cf67736f19e1af54d322
twiemold <thomas.wiemold@gmail.com>
2023-07-12 18:22:46 -0400

    Create test case

--- day-one/app/Main.hs
+++ day-one/app/Main.hs
@@ -3,7 +3,17 @@ import Text.Read (readMaybe)
 import Data.Maybe (mapMaybe)
 
 main :: IO ()
+fuelCalc :: [Int] -> Int
+
+fuelCalc x = sum $ map (\y -> div y 3 - 2) $ x
 readMaybeInt = readMaybe :: String -> Maybe Int
+
+test = do
+    print "hi"
+    print . fuelCalc $ [100756]
+
 main = do
+    test
     contents <- readFile "app/input.txt"
-    print . sum $ map (\x -> div x 3 - 2) . mapMaybe readMaybeInt . words $ contents
+    print . fuelCalc . mapMaybe readMaybeInt . words $ contents
+
